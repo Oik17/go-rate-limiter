f42447af3794c70c26d0e74c69c5cb8ec0421fa4
Akshat Gupta <akshat.g1707@gmail.com>
2024-11-02 18:43:54 +0530

    feat: per client rate limiter

--- per-client-rate-limiting/go.mod
+++ per-client-rate-limiting/go.mod
@@ -1,3 +1,5 @@
 module github.com/oik17/per-client
 
 go 1.23.1
+
+require golang.org/x/time v0.7.0
--- per-client-rate-limiting/main.go
+++ per-client-rate-limiting/main.go
@@ -1,14 +1,88 @@
-package main   
+package main
 
-import("
+import (
+	"encoding/json"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+	"time"
 
-")
+	"golang.org/x/time/rate"
+)
 
 type Message struct {
 	Status string `json:"status"`
 	Body   string `json:"body"`
 }
 
-func main(){
-	
+func perClientRateLimiter(next func(writer http.ResponseWriter, request *http.Request)) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr) //Get the IP address of the client
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+			message := Message{
+				Status: "Request Failed",
+				Body:   "API is at capacity",
+			}
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		}
+		mu.Unlock()
+
+		next(w, r)
+	})
+
+}
+
+func endpointHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	message := Message{
+		Status: "success",
+		Body:   "Successfully pinged",
+	}
+	err := json.NewEncoder(writer).Encode(message)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+func main() {
+	http.HandleFunc("/ping", perClientRateLimiter(endpointHandler).ServeHTTP)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Println("Error starting server", err)
+	}
+
 }
